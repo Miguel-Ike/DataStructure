c1e44b18ad045323970170f9182b3bfd41fc9300
Miguel Angel Martinez Rodriguez <miguel_angel-m@outlook.com>
2024-02-12 21:09:46 -0400

    Initial commit

---
+++ src/main/java/app/DataStructure.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app;
+
+/**
+ *
+ * @author Miguel_Angel
+ */
+public class DataStructure {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
